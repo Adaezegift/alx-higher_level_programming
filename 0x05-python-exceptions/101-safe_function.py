c427385eaf45fddd7c13372f07d8203f3e5288c8
adaezegift <adaezegift@yahoo.com>
2023-03-28 03:18:02 -0700

    updated

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+        """Executes a function safely.
+
+            Args:
+                    fct: The function to execute.
+                            args: Arguments for fct.
